c058f27cb253995bff877c447d1774e5091b6b12
Panter530xd <bobansenic@gmail.com>
2023-07-12 20:11:15 +0200

    Added linkin logo

--- platform/components/heder/Heder.tsx
+++ platform/components/heder/Heder.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Logo from "../../public/svg/logo.svg";
+import Link from "next/link";
 export default function Heder() {
   return (
     <div className="w-11/12 max-w-screen-xl mx-auto pt-10 ">
+      <Link href={"/"} >
       <Image
         src={Logo.src}
         alt={"Logo"}
@@ -11,6 +13,8 @@ export default function Heder() {
         priority
         className="ml-auto lg:w-20 w-16 h-auto"
       />
+      </Link>
+    
     </div>
   );
 }
